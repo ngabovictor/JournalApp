486faafcaed75f32935bcd769ef5897716f3cc17
victor <nvichack@gmail.com>
2018-06-26 01:16:14 +0200

    Added drawables, designed the login activity

--- app/src/main/java/com/corelabsplus/journalapp/activities/SplashActivity.java
+++ app/src/main/java/com/corelabsplus/journalapp/activities/SplashActivity.java
@@ -1,19 +1,41 @@
 package com.corelabsplus.journalapp.activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.corelabsplus.journalapp.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private Context context;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        context = this;
+
+        mAuth = FirebaseAuth.getInstance();
 
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //Check for current user
+
+        if (mAuth.getCurrentUser() == null){
+            Intent intent = new Intent(context, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 }
